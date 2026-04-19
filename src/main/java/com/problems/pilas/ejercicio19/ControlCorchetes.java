9cf3a5d6b1bd2350fb2ac2743c9373cc7c5049e2
NicolasDiazAg <NICOLAS.DIAZS@correo.ucu.edu.uy>
2026-04-19 18:14:14 -0300

    ej19

--- src/main/java/com/problems/pilas/ejercicio19/ControlCorchetes.java
+++ src/main/java/com/problems/pilas/ejercicio19/ControlCorchetes.java
@@ -7,13 +7,28 @@ public class ControlCorchetes {
 
     public static void ejecutar19(String[] args) {
         List<Character> listaDeEntrada = List.of('{', '{', '}', '}');
+        List<Character> listaDeEntrada2 = List.of('{', '}', '}', '{');
         boolean resultado = controlCorchetes(listaDeEntrada);
-        System.out.println("¿Los corchetes están correctamente balanceados? " + resultado);
+        System.out.println("¿Los corchetes estan correctamente? " + resultado);
+        boolean resultado2 = controlCorchetes(listaDeEntrada2);
+        System.out.println("¿Los corchetes de la segunda lista estan correctamente? " + resultado2);
     }
 
     public static boolean controlCorchetes(List<Character> listaDeEntrada) {
         Stack<Character> pila = new Stack<>(); // Stack para almacenar los corchetes de apertura
-            
+            for (Character caracter : listaDeEntrada) {
+                if (caracter == '{') {
+                    pila.push(caracter);
+                } else if (caracter == '}') {
+                    if (pila.isEmpty()) {
+                        return false; // No hay un corchete de apertura para este cierre
+                    }
+                    char ultimoCorchete = pila.pop();
+                    if (ultimoCorchete != '{') {
+                        return false; // El corchete de cierre no corresponde al de apertura
+                    }
+                }
+            }
         return pila.isEmpty();
     }
 
