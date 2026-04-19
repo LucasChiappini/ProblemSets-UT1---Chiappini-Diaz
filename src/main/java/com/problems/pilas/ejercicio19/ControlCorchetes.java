2a16e223bd73691363075b2a84763e53234ddf16
NicolasDiazAg <NICOLAS.DIAZS@correo.ucu.edu.uy>
2026-04-19 18:04:00 -0300

    ej19

---
+++ src/main/java/com/problems/pilas/ejercicio19/ControlCorchetes.java
@@ -0,0 +1,14 @@
+package com.problems.pilas.ejercicio19;
+ 
+import java.util.List;
+import java.util.Stack;
+
+public class ControlCorchetes {
+
+    public static boolean controlCorchetes(List<Character> listaDeEntrada) {
+        Stack<Character> pila = new Stack<>(); // Stack para almacenar los corchetes de apertura
+        
+        return pila.isEmpty();
+    }
+
+}
--- src/main/java/com/problems/pilas/ejercicio19/Ejercicio19.java
+++ src/main/java/com/problems/pilas/ejercicio19/Ejercicio19.java
@@ -4,6 +4,7 @@ public class Ejercicio19 {
 
     public static void ejecutar() {
         System.out.println("--- Iniciando Ejercicio 19 ---");
+        
 
         System.out.println("Ejercicio 19 finalizado con éxito.");
     }
